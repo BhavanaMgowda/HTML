794358cd2b86c020d15e746cecf9e4a444283bc9
bhavanagowda04 <117250531+bhavanagowda04@users.noreply.github.com>
2023-04-18 10:40:26 +0530

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -1,4 +1,12 @@
-let num1 = 10;
-let num2 = 5;
-let result = num1 * num2;
-console.log(result); 
+// Import the Fraction object from fraction.js
+const Fraction = require('fraction.js');
+
+// Define two fractions to subtract
+const frac1 = new Fraction(3, 4);
+const frac2 = new Fraction(1, 2);
+
+// Perform subtraction
+const result = frac1.sub(frac2);
+
+// Display the result
+console.log(`The result of subtracting ${frac2} from ${frac1} is: ${result}`);
